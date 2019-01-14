37db5fda876e0699a102e4a5fca5cf00bbf9865d
Matthew Mosesohn <matthew.mosesohn@gmail.com>
2019-01-14 19:06:37 +0300

    Improve reminder message text

--- emailscript/send_email.py
+++ emailscript/send_email.py
@@ -8,6 +8,7 @@ import time
 instance_info_path = os.environ.get('INSTANCE_INFO_PATH', '/config/instance-info.json')
 initial_delay = int(os.environ.get('INITIAL_DELAY', 86400))  # 1 day by default
 notify_interval = int(os.environ.get('NOTIFY_INTERVAL', 604800))  # 1 week by default
+smtp_debuglevel = int(os.environ.get('SMTP_DEBUGLEVEL', 0))  # 1 week by default
 
 smtp_server_credentials = {
     'server': os.environ.get('SMTP_SERVER'),
@@ -22,33 +23,34 @@ def send_email(recipient, email_body):
     s = smtplib.SMTP_SSL(smtp_server_credentials['server'], smtp_server_credentials['port'])
     s.ehlo()
     s.login(smtp_server_credentials['login'], smtp_server_credentials['password'])
-    s.set_debuglevel(1)
+    s.set_debuglevel(smtp_debuglevel)
 
     msg = MIMEText(email_body)
-    msg['Subject'] = "Check your instances"
+    msg['Subject'] = "[ATOM Infra] Reminder: You have a running cluster"
     msg['From'] = smtp_server_credentials['sender']
     msg['To'] = recipient
 
-    print "Sending..."
-    print msg
+    print("Sending...")
+    print(msg)
 
     s.sendmail(smtp_server_credentials['sender'], recipient, msg.as_string())
 
-    print "Message has been sent"
+    print("Message has been sent")
 
 
 with open(instance_info_path) as f:
     instance = json.load(f)
 
 instances = instance['InstanceIds']
-instances.pop()
 
 time.sleep(initial_delay)
 region = instance['Region']
 recipient = instance['Email']
 email_body = "You have running instances in " + region + " region.\n"\
-             "Instances: " + ', '.join(instances) + ".\n" \
-             "Please check if they are still in use. Thank you.\n"
+             "Instances: " + ', '.join(instances) + ".\n\n" \
+             "If you are no longer using these resources, please "\
+             "tear down your cluster. https://github.com/atom-infra/atom-infra#teardown.\n\n"\
+             "Regards,\nATOM Infra Team"
 
 while True:
     send_email(recipient, email_body)
